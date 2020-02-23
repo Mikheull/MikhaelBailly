aed541baafbefc67accb23f271ad7a7863c1e071
Mikheull <mikhael.bailly1@orange.fr>
2020-02-23 02:35:21 +0100

    add: projects

--- routes/project.js
+++ routes/project.js
@@ -1,16 +1,47 @@
 let express = require('express');
 let router = express.Router();
-
+const fs = require("fs");
 
 /* GET project home page. */
 router.get('/', async function(req, res, next) {
-	res.send('project home');
+	let rawdata = fs.readFileSync('data/projects.json');
+	
+	res.render('index', {
+		viewPath: 'project/list.ejs',
+		currentPage: 'project',
+		projectsData: JSON.parse(rawdata)
+	});
 });
-
 
 /* GET project reference page. */
 router.get('/:query', async function (req, res) {
-	res.send(req.params.query);
+	let ref = req.params.query;
+	let rawdata = fs.readFileSync('data/projects.json');
+	let list = JSON.parse(rawdata);
+
+	let code = '';
+	for (let key in list) {
+		if(list[key].link == ref){
+			code = key;
+		}
+	}
+	
+	if(code !== ''){
+		let rawdata_p = fs.readFileSync('data/'+code+'.json');
+		res.render('index', {
+			viewPath: 'project/article.ejs',
+			currentPage: 'project',
+			projectData: JSON.parse(rawdata_p),
+			ref: code
+		});
+	}else{
+		res.render('index', {
+			viewPath: 'project/article_not_found.ejs',
+			currentPage: 'project',
+		});
+	}
+	
+
 })
 
 module.exports = router;
--- routes/routes.js
+++ routes/routes.js
@@ -5,13 +5,12 @@ const router = express.Router();
 router.get('/', async function(req, res, next) {
 	res.render('index', {
 		viewPath: 'home/index.ejs',
-		currentPage: 'home',
-		assets: {stylesheets: ['home.css']},
+		currentPage: 'home'
 	});
 });
 
 router.use('/github', require('./github') );
-router.use('/project', require('./project') );
+router.use('/projets', require('./project') );
 
 router.use(function(req,res){
     res.status(404).send('404');
