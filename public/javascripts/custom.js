195efccfd6f18721ff96c2726572ee9c1d3e8d24
Mikheull <mikhael.bailly1@orange.fr>
2020-02-23 02:49:25 +0100

    add: AOS animations

--- public/javascripts/custom.js
+++ public/javascripts/custom.js
@@ -2,3 +2,6 @@ function toggleNavbar(collapseID) {
     document.getElementById(collapseID).classList.toggle("hidden");
     document.getElementById(collapseID).classList.toggle("block");
 }
+
+
+AOS.init();
