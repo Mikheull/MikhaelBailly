04d6c4f1795775d20ac1635401e181907331f04f
Mikheull <mikhael.bailly1@orange.fr>
2020-03-13 22:05:20 +0100

    remove: email contact

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,5 @@
 let express = require('express');
 let router = express.Router();
-const nodemailer = require('nodemailer');
 
 router.get('/', async function(req, res, next) {
 	res.render('index', {
@@ -9,37 +8,6 @@ router.get('/', async function(req, res, next) {
 		baseUri: process.env.baseUri
 	});
 });
-
-router.post('/', async function(req, res, next) {
-	let transport = nodemailer.createTransport({
-        host: 'smtp.mailtrap.io',
-        port: 2525,
-        auth: {
-           user: process.env.MAILTRAP_USER,
-           pass: process.env.MAILTRAP_PASS
-        }
-    });
-
-    if(req.body.email && req.body.message && req.body['g-recaptcha-response'] !== '') {
-        const message = `Mail: ${req.body.email}<br> Nom: ${req.body.full_name}<br><br> Message: ${req.body.message}`;
-        const email = {
-            from: 'mikhae.bailly@gmail.com',
-            to: 'mikhae.bailly@gmail.com',
-            subject: 'Contact ['+ req.body.email +']',
-            html: message
-        };
-        transport.sendMail(email, function(err, info) {
-            if (err) {
-                console.log(err)
-            } else {
-                console.log(info);
-            }
-        });
-    }
-
-	res.redirect('/');
-});
-
 
 
 module.exports = router;
